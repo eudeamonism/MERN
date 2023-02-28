1365a881a54f6b800f068552bb0e6ae1ee85eefa
Evan Vargas <evanvargas@gmail.com>
2023-02-27 20:28:10 -0600

    Project 2

--- front/src/App.js
+++ front/src/App.js
@@ -1,74 +1,18 @@
-import React, { useState, useEffect } from 'react';
-
-import Header from './components/Header/Header';
-import NewProduct from './components/Products/NewProduct';
-import ProductList from './components/Products/ProductList';
-import './App.css';
+import React from 'react'
+import classes from './App.module.css'
 
 function App() {
-  const [loadedProducts, setLoadedProducts] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      setIsLoading(true);
-      const response = await fetch('http://localhost:5000/products');
-
-      const responseData = await response.json();
-
-      setLoadedProducts(responseData.products);
-      setIsLoading(false);
-    };
-
-    fetchProducts();
-  }, []);
-
-  const addProductHandler = async (productName, productPrice) => {
-    try {
-      const newProduct = {
-        title: productName,
-        price: +productPrice // "+" to convert string to number
-      };
-      let hasError = false;
-      const response = await fetch('http://localhost:5000/product', {
-        method: 'POST',
-        body: JSON.stringify(newProduct),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-
-      if (!response.ok) {
-        hasError = true;
-      }
-
-      const responseData = await response.json();
-
-      if (hasError) {
-        throw new Error(responseData.message);
-      }
-
-      setLoadedProducts(prevProducts => {
-        return prevProducts.concat({
-          ...newProduct,
-          id: responseData.product.id
-        });
-      });
-    } catch (error) {
-      alert(error.message || 'Something went wrong!');
-    }
-  };
-
   return (
-    <React.Fragment>
-      <Header />
-      <main>
-        <NewProduct onAddProduct={addProductHandler} />
-        {isLoading && <p className="loader">Loading...</p>}
-        {!isLoading && <ProductList items={loadedProducts} />}
-      </main>
-    </React.Fragment>
-  );
+      <div className={classes['coarse-goals']}>
+          <h2>Course Goals</h2>
+          <ul className={classes['goal-list']}>
+          <li>Finish the Course</li>
+          <li>Learn all about the Course's Main Topic</li>
+          <li>Help Other Students</li>
+          </ul>
+
+      </div>
+  )
 }
 
 export default App;
