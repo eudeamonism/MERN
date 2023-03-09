8aa1d99f04e90b2de1c4f82774f6b9177cf31cb7
Evan Vargas <evanvargas@gmail.com>
2023-03-08 18:29:00 -0600

    prior to Error Model

--- back/app.js
+++ back/app.js
@@ -3,7 +3,6 @@ const express = require('express');
 
 const placesRoutes = require('./routes/places-routes');
 
-
 koopa = process.env.PORT;
 const app = express();
 
@@ -12,5 +11,14 @@ const app = express();
 //Route: we applied a new route to our placeRoutes
 app.use('/api/places', placesRoutes);
 
+//Error handling Middleware for our routes
+app.use((error, req, res, next) => {
+	if (res.headersSent) {
+		return next(error);
+	}
+
+	res.status(error.code || 500);
+	res.json({ message: error.message || 'An unknown error occurred!' });
+});
 
 app.listen(koopa);
--- back/routes/places-routes.js
+++ back/routes/places-routes.js
@@ -15,13 +15,20 @@ const DUMMY_PLACES = [
 		creator: 'u1',
 	},
 ];
-
+//This has to be first. Examine the second link.
+//if we had a route instead like /user, such would need to be before this or it would never be reached.
 router.get('/:pid', (req, res, next) => {
 	const placeId = req.params.pid; // { pid: 'p1' }
 
 	const place = DUMMY_PLACES.find((p) => {
 		return p.id === placeId;
 	});
+
+	if (!place) {
+		const error = new Error('Could not find a place for provided id.');
+		error.code = 404;
+		throw error;
+	}
 
 	res.json({ place }); // => { place } => { place: place }
 });
@@ -32,6 +39,12 @@ router.get('/user/:uid', (req, res, next) => {
 	const place = DUMMY_PLACES.find((p) => {
 		return p.creator === userId;
 	});
+
+	if (!place) {
+		const error = new Error('Could not find a place for provided user id.');
+		error.code = 404;
+		return next(error);
+	}
 
 	res.json({ place });
 });
